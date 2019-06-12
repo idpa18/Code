32986ea9b2894c6180a8645d51c5a7120303a41c
Seya Schmassmann <=>
2019-06-12 16:42:57 +0700

    + routing reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { MilitaryFortressComponent } from './views/military-fortress/military-fortress.component';
 import { PersonOverviewComponent } from './views/person/person-overview/person-overview.component';
 import { PersonDetailComponent } from './views/person/person-detail/person-detail.component';
+import { MenuComponent } from './menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MilitaryFortressComponent,
     PersonOverviewComponent,
-    PersonDetailComponent
+    PersonDetailComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
