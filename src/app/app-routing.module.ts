0e6719f97cb1f695251cbd990cfa58a40639601c
Dario Wigger <KAIROS\Dario.Wigger>
2019-06-12 16:22:49 +0200

    + router config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MilitaryFortressComponent } from './views/military-fortress/military-fortress.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MilitaryFortressComponent } from "./views/military-fortress/military-fortress.component";
+import { PersonOverviewComponent } from "./views/person/person-overview/person-overview.component";
 
 const routes: Routes = [
-  { path: 'festung-vitznau', component: MilitaryFortressComponent }
-];  
+  { path: "festung-vitznau", component: MilitaryFortressComponent },
+  { path: "personen", component: PersonOverviewComponent }
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
