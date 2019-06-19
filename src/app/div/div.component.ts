89803aa7feb8df9010e281a435985b58501b03f8
Stefanie Jäger <stefijaeger@gmail.com>
2019-06-19 17:16:25 +0200

    Make scrollspy work

--- src/app/div/div.component.ts
+++ src/app/div/div.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-div',
   templateUrl: './div.component.html',
   styleUrls: ['./div.component.scss']
 })
-export class DivComponent implements OnInit {
+export class DivComponent implements OnInit, AfterViewInit {
+  fragment: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
+  }
+
+  ngAfterViewInit(): void {
+    this.navigateTo(this.fragment);
+  }
+
+  navigateTo(fragment: string) {
+    try {
+      console.log(fragment);
+      document.querySelector('#' + fragment).scrollIntoView();
+    } catch (e) { }
   }
 
 }
